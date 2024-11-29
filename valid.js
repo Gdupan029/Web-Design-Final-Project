5a13ef0560adbe4b2dcc673568ce6e569ab3cc27
Gdupan029 <dupangerald29@gmail.com>
2024-11-29 22:01:16 +0800

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,14 @@
+function shubha() {
+    name = document.forms[form1][fname].value;
+    pass = document.forms[form1][pwd].value;
+
+    if (name == "")
+{
+    alert("Please enter username..")
+}
+    if (pass == "")
+{
+    alert("Please enter password..")
+}
+
+}
